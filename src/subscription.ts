d4b5467bdd481490b07442b2ecfa3eed2db76eee
夏雨 <63986405+xy-x@users.noreply.github.com>
2026-02-20 13:15:39 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,16 +1,28 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
 import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
-import globalGroups from './globalGroups';
+import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+const rawApps: RawApp[] = [];
+apps.forEach((appConfig) => {
+  appConfig.groups?.forEach((g: RawAppGroup) => {
+    if (g.name.startsWith('开屏广告')) {
+      g.order = OPEN_AD_ORDER;
+    }
+  });
+  rawApps.push(appConfig);
+});
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 666,
+  name: ' MY GKD订阅',
   version: 0,
-  author: 'author',
+  author: 'xy',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
   globalGroups,
-  apps: await batchImportApps(`${import.meta.dirname}/apps`),
+  apps: rawApps,
 });
